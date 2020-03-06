78254e34dd5e0da97755d291e850438010c86ccc
Skepickle <skepickle@users.noreply.github.com>
2020-03-06 01:45:41 -0500

    Fix spell specification for Entropic Shield.

--- skepickleCharacterSuite/skepickleCharacterSuite_SRD.js
+++ skepickleCharacterSuite/skepickleCharacterSuite_SRD.js
@@ -3597,6 +3597,7 @@ var skepickleCharacterSuite_SRD = skepickleCharacterSuite_SRD || (function skepi
         components:       'V, S',
         casting_time:     '1 standard action',
         range:            'Personal',
+        target_type:      'Target',
         target:           'You',
         duration:         '[[?{Caster Level}]] minutes (D)',
         text:             `A magical field appears around you, glowing with a chaotic blast of multicolored hues. This field deflects incoming arrows, rays, and other ranged attacks. Each ranged attack directed at you for which the attacker must make an attack roll has a 20% miss chance (similar to the effects of concealment). Other attacks that simply work at a distance are not affected.`
